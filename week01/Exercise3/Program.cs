8fcb2600b38088c236f5ea72efda263df9f78ab1
ahiabormensah <ahiamensah927@gmail.com>
2025-07-04 12:28:03 +0000

    Number Guess

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,32 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101); // 1 to 100
+
+        int guess = -1;
+
+        Console.WriteLine("Welcome to Guess My Number!");
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
