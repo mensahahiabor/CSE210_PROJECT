d4b3d75877773c177ad9773839815a3a4287ab36
ahiabormensah <ahiamensah927@gmail.com>
2025-07-04 12:13:11 +0000

    Percentage Grader

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,12 +4,42 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Enter your first name: ");
-        string firstName = Console.ReadLine();
+        Console.Write("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
 
-        Console.Write("Enter your last name: ");
-        string lastName = Console.ReadLine();
+        string letter = "";
 
-        Console.WriteLine($"Your name is {lastName}, {firstName}, {lastName}");
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Don't worry, keep working hard and you'll do better next time!");
+        }
     }
 }
